0f0a46aeca5e493f2eacb8d07bc69d4dd3658d70
jingxin-zeng <jingxin.zeng@suncode-smart.com>
2026-01-29 22:39:09 +0800

    Initial Commit

---
+++ AtlasDataProcessorPlus/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AtlasDataProcessorPlus
+//
+//  Created by gdlocal on 2026/1/29.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
